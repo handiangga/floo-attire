d7cb776365c558bd1bc220a7a51ae7d9c578c3e1
unknown <wadangalap@gmail.com>
2026-06-12 14:40:02 +0700

    ganti link lagi

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -98,7 +98,7 @@ export default function Collection() {
 
         {/* CTA Button */}
         <a
-          href="https://drive.google.com/file/d/1rdbcYbHEnhLhBYSzPKpbnxPLA9MNaHZJ/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/1sVY2go6eC33i3qEQdtaUgd9sL9gJsqMn/view?usp=drivesdk"
           target="_blank"
           rel="noopener noreferrer"
           className="
--- src/components/mainCTA.tsx
+++ src/components/mainCTA.tsx
@@ -42,7 +42,7 @@ export default function MainCTA() {
       <div className="relative z-10">
         {/* Main CTA */}
         <a
-          href="https://drive.google.com/file/d/1rdbcYbHEnhLhBYSzPKpbnxPLA9MNaHZJ/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/1sVY2go6eC33i3qEQdtaUgd9sL9gJsqMn/view?usp=drivesdk"
           target="_blank"
           rel="noopener noreferrer"
           className="
